d1175fb8a5959fd6a5ecd2c4db59a23b0de402de
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-07-20 22:25:41 +0530

    update

--- dist/Icons/Accessibility.svelte.d.ts
+++ dist/Icons/Accessibility.svelte.d.ts
@@ -1,7 +1,7 @@
 /** @typedef {typeof __propDef.props}  AccessibilityProps */
 /** @typedef {typeof __propDef.events}  AccessibilityEvents */
 /** @typedef {typeof __propDef.slots}  AccessibilitySlots */
-export default class Accessibility extends SvelteComponent<{
+export default class Accessibility extends SvelteComponentTyped<{
     [x: string]: any;
     color?: string | undefined;
     size?: number | undefined;
@@ -12,7 +12,7 @@ export default class Accessibility extends SvelteComponent<{
 export type AccessibilityProps = typeof __propDef.props;
 export type AccessibilityEvents = typeof __propDef.events;
 export type AccessibilitySlots = typeof __propDef.slots;
-import { SvelteComponent } from "svelte";
+import { SvelteComponentTyped } from "svelte";
 declare const __propDef: {
     props: {
         [x: string]: any;
